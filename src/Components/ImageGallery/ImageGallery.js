d2ed6bf2dabcd3165a053f4528f005c26f4c2dc7
“samparkk13” <sam13park@gmail.com>
2024-08-09 13:10:30 -0400

    image gallery resize

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,43 +1,58 @@
 import React, { useState, useEffect } from 'react';
 
 const images = [
-  'Images/front.jpg', 
-  'Images/bagels.jpg', 
-  'Images/preBagel.jpg', 
+  'Images/front.jpg',
+  'Images/bagels.jpg',
+  'Images/preBagel2.jpg',
   'Images/hotFood.jpg',
   'Images/page1.jpg',
   'Images/page2.jpg',
   'Images/page3.jpg',
-  'Images/insideFront.jpg',
   'Images/sideStore.jpg',
 ];
 
 const ImageGallery = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
-      }, 10000); // Change image every 10 seconds
-  
-      return () => clearInterval(interval);
-    }, []);
-  
-    const goToPrevious = () => {
-      setCurrentIndex(prevIndex => (prevIndex - 1 + images.length) % images.length);
-    };
-  
-    const goToNext = () => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
-    };
-  
-    return (
-      <div className="image-gallery">
-        <button className="left-arrow" onClick={goToPrevious}>&lt;</button>
-        <img src={images[currentIndex]} alt="Gallery" className="gallery-image" />
-        <button className="right-arrow" onClick={goToNext}>&gt;</button>
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 10000); // Change image every 10 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const goToImage = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="image-gallery">
+      <button className="left-arrow" onClick={goToPrevious}>&lt;</button>
+      <img src={images[currentIndex]} alt="Gallery" className="gallery-image" />
+      <button className="right-arrow" onClick={goToNext}>&gt;</button>
+
+      <div className="image-thumbnails">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Thumbnail ${index}`}
+            className={`thumbnail ${index === currentIndex ? 'active' : ''}`}
+            onClick={() => goToImage(index)}
+          />
+        ))}
       </div>
-    );
-  }
-  
-  export default ImageGallery;
+    </div>
+  );
+};
+
+export default ImageGallery;
