c39df1a02ac247062cb4f5541892703b35d9df1b
“samparkk13” <sam13park@gmail.com>
2024-08-16 12:38:18 -0400

    updated navbar font

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="links">
-        <a href="#home"><h1>Home</h1></a>
-        <a href="#about"><h1>About</h1></a>
-        <a href="#menu"><h1>Menu</h1></a>
-        <a href="#contact"><h1>Contact</h1></a>
+        <a href="#home"><h1>HOME</h1></a>
+        <a href="#about"><h1>ABOUT</h1></a>
+        <a href="#menu"><h1>MENU</h1></a>
+        <a href="#contact"><h1>CONTACT</h1></a>
       </div>
     </nav>
   );
