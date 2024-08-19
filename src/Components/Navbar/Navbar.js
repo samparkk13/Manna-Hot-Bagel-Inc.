2a3d98788d897bb127e28aeb90ba1c49c8bf8c09
“samparkk13” <sam13park@gmail.com>
2024-08-19 17:23:29 -0400

    added online orders

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = () => {
         <a href="#about"><h1>ABOUT</h1></a>
         <a href="#menu"><h1>MENU</h1></a>
         <a href="#contact"><h1>CONTACT</h1></a>
+        <a href="https://www.ubereats.com/store/manna-hot-bagel/LDjkSj4lX3-iKYuAUER-cA?srsltid=AfmBOopU2085BrtXsQ8_uTFIcEl60hECjBI1cSv_IV_naGYwzuBy_7ay" target="_blank" rel="noopener noreferrer">
+          <h1>ORDER</h1>
+        </a>
       </div>
     </nav>
   );
